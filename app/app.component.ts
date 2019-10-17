d180a7d8d26198a47d49033c62c748d386742d84
Ivan Radchenko <ivan_radchenko@epam.com>
2019-10-18 00:58:28 +0300

    Add template reference binding vs reactiveBinding

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,25 +8,39 @@ import { Component } from '@angular/core';
       {{title}}
     </div>
     <div>
+      <h1>Explicit two-way data binding</h1>
       <input
       type="text"
       [ngModel]="name"
       (ngModelChange)="handleChange($event)"
       />
+    <div>
+    <h1>Implicit two-way data binding</h1>
       <input
       type="text"
       [(ngModel)]="name"
       />
-      <p>{{name}}</p>
+    </div>
+    <div>
+    <h1>Template reference binding</h1>
+      <input
+      type="text"
+      #username
+      />
+    </div>
+      <p *ngIf="name">
+        Searching for ...  {{name}}
+      </p>
     </div>
     <div>
     <button type="button" (click)="handleClick()">Click Me</button>
+    <button type="button" (click)="onClick(username.value)">Get value from Username</button>
     </div>
   `
 })
 export class AppComponent {
   title: string;
-  name: string = 'Ivan';
+  name: string = '';
   constructor(){
     this.title = 'Hello world';
   }
@@ -37,5 +51,10 @@ export class AppComponent {
 
   handleChange(value: string) {
     this.name = value;
+  }
+
+  onClick(value: string) {
+    this.name = value;
+    console.log(value);
   }
 }
