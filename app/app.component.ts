d3e9b41e11a4d8d42ad073f3c84019eba621be35
Ivan Radchenko <ivan_radchenko@epam.com>
2019-10-18 00:10:29 +0300

    Angular fundamentals two-way data binding implicit/explicit with ngModel

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,37 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{title}}
+    </div>
+    <div>
+      <input
+      type="text"
+      [ngModel]="name"
+      (ngModelChange)="handleChange($event)"
+      />
+      <input
+      type="text"
+      [(ngModel)]="name"
+      />
+      <p>{{name}}</p>
+    </div>
+    <div>
+    <button type="button" (click)="handleClick()">Click Me</button>
     </div>
   `
 })
 export class AppComponent {
+  title: string;
+  name: string = 'Ivan';
+  constructor(){
+    this.title = 'Hello world';
+  }
 
+  handleClick(){
+    this.name = "SomeName";
+  }
+
+  handleChange(value: string) {
+    this.name = value;
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
-    AppComponent
-  ]
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
+
